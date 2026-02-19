8bffc804b712c206b5cd521a0ec5727bce99504e
biankakrysiak <krysiak-bianka@wp.pl>
2026-02-19 22:20:11 +0100

    Initial commit

---
+++ Chess/move.py
@@ -0,0 +1,35 @@
+class Move:
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+        # compare 1 integer instead of 4, faster
+        self.moveID = (
+            self.startRow * 1000 +
+            self.startCol * 100 +
+            self.endRow * 10 +
+            self.endCol
+        )
+
+    def __eq__(self, other):
+        return isinstance(other, Move) and self.moveID == other.moveID
+
+    def __str__(self):
+        return f"{self.pieceMoved}: ({self.startRow},{self.startCol}) -> ({self.endRow},{self.endCol})"
+    
+
+'''
+moveID xample
+(startRow)(startCol)(endRow)(endCol)
+(6,4) -> (4,4)
+6*1000 = 6000
+4*100  = 400
+4*10   = 40
+4      = 4
+         6444
+'''
